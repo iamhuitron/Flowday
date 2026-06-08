e091a7d0a5eee0a6ae18779ce034703e4a463fb6
Ian Miguel Huitron <ianhuitron0687@gmail.com>
2026-06-07 20:23:48 -0600

    babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
-    ],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './src',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@store': './src/store',
-            '@hooks': './src/hooks',
-            '@utils': './src/utils',
-            '@types': './src/types',
-            '@constants': './src/constants',
-          },
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
+    presets: ['babel-preset-expo'],
+    plugins: [], 
   };
 };
