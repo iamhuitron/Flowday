45511a2185225c4208415f3fa0b79ce1d8d2670f
Ian Miguel Huitron <ianhuitron0687@gmail.com>
2026-06-09 18:58:22 -0600

    sin bundle

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Resolver los alias definidos en tsconfig.json
+config.resolver.extraNodeModules = {
+  '@': path.resolve(__dirname, 'src'),
+  '@components': path.resolve(__dirname, 'src/components'),
+  '@screens': path.resolve(__dirname, 'src/screens'),
+  '@store': path.resolve(__dirname, 'src/store'),
+  '@hooks': path.resolve(__dirname, 'src/hooks'),
+  '@utils': path.resolve(__dirname, 'src/utils'),
+  '@types': path.resolve(__dirname, 'src/types'),
+  '@constants': path.resolve(__dirname, 'src/constants'),
+};
 
 module.exports = withNativeWind(config, { input: './global.css' });
